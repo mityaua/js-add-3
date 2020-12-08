715e2c4084cd659cdff3b59137e9fe38f9a382c8
Dmytro <mityaua@gmail.com>
2020-12-08 23:29:01 +0200

    Some fixes

--- js/3-12.js
+++ js/3-12.js
@@ -1,16 +1,32 @@
+// Второй вариант решения
+
 function countProps(object) {
   let propCount = 0;
   // Пиши код ниже этой строки
-  const arr = [];
   for (const key in object) {
     if (object.hasOwnProperty(key)) {
-      arr.push(key);
-      propCount = arr.length;
+      propCount += 1;
     }
   }
   // Пиши выше ниже этой строки
-  return console.log(propCount);
+  return propCount;
 }
+
+// // Первый вариант решения
+
+// function countProps(object) {
+//   let propCount = 0;
+//   // Пиши код ниже этой строки
+//   const arr = [];
+//   for (const key in object) {
+//     if (object.hasOwnProperty(key)) {
+//       arr.push(key);
+//       propCount = arr.length;
+//     }
+//   }
+//   // Пиши выше ниже этой строки
+//   return console.log(propCount);
+// }
 
 countProps({}); // возвращает 0.
 countProps({ name: 'Mango', age: 2 }); // возвращает 2.
