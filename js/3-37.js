dfd47140cdfb015333e53c9513b470c8e3a33ae3
Dmytro <mityaua@gmail.com>
2020-12-10 12:25:58 +0200

    Добавляет решение 38 задачи и условие 39

--- js/3-37.js
+++ js/3-37.js
@@ -1,5 +1,10 @@
 const atTheOldToad = {
   // Пиши код ниже этой строки
   potions: ['Зелье скорости', 'Дыхание дракона', 'Каменная кожа'],
+  getPotions(potions) {
+    return this.potions;
+  },
   // Пиши код выше этой строки
 };
+
+// Добавь объекту atTheOldToad метод getPotions(), который просто возвращает значение свойства potions.
---
+++ js/3-38.js
@@ -0,0 +1,10 @@
+const atTheOldToad = {
+  potions: ['Зелье скорости', 'Дыхание дракона', 'Каменная кожа'],
+  addPotion(potionName) {
+    // Пиши код ниже этой строки
+    this.potions.push(potionName);
+    // Пиши код выше этой строки
+  },
+};
+
+// Дополни метод addPotion(potionName) так, чтобы он добавлял зелье potionName в конец массива зелий в свойстве potions.
