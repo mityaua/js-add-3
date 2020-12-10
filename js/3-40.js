77b44f757030668cf22be12f872d1f5660504659
Dmytro <mityaua@gmail.com>
2020-12-10 12:35:59 +0200

    Добавляет решение 40й задачи и условие 41

---
+++ js/3-40.js
@@ -0,0 +1,11 @@
+const atTheOldToad = {
+  potions: ['Зелье скорости', 'Дыхание дракона', 'Каменная кожа'],
+  updatePotionName(oldName, newName) {
+    // Пиши код ниже этой строки
+    const potionIndex = this.potions.indexOf(oldName);
+    this.potions.splice(potionIndex, 1, newName);
+    // Пиши код выше этой строки
+  },
+};
+
+// Дополни метод updatePotionName(oldName, newName) так, чтобы он обновлял название зелья с oldName на newName, в массиве зелий в свойстве potions.
